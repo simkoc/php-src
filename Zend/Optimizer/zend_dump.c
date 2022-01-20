dc79edf823950368e45f54a7055513b448d83054
Simon Koch <simon.koch@tu-braunschweig.de>
2022-01-20 16:30:26 +0100

    implemented base64 encoding of opcache string dump

--- Zend/Optimizer/zend_dump.c
+++ Zend/Optimizer/zend_dump.c
@@ -24,6 +24,190 @@
 #include "zend_call_graph.h"
 #include "zend_dump.h"
 
+
+////// THIS CONTENT IS FROM b64 copyright (c) 2014 joseph werle MIT LICENSE
+
+#ifndef B64_H
+#define B64_H 1
+
+/**
+ *  Memory allocation functions to use. You can define b64_malloc and
+ * b64_realloc to custom functions if you want.
+ */
+
+#ifndef b64_malloc
+#  define b64_malloc(ptr) malloc(ptr)
+#endif
+#ifndef b64_realloc
+#  define b64_realloc(ptr, size) realloc(ptr, size)
+#endif
+
+ // How much memory to allocate per buffer
+#define B64_BUFFER_SIZE		(1024 * 64) // 64K
+
+ // Start buffered memory
+char* b64_buf_malloc();
+
+// Update memory size. Returns the same pointer if we
+// have enough space in the buffer. Otherwise, we add
+// additional buffers.
+char* b64_buf_realloc(unsigned char* ptr, size_t size);
+
+/**
+ * Base64 index table.
+ */
+
+static const char b64_table[] = {
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+  'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+  'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+  'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+  'w', 'x', 'y', 'z', '0', '1', '2', '3',
+  '4', '5', '6', '7', '8', '9', '+', '/'
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Encode `unsigned char *' source with `size_t' size.
+ * Returns a `char *' base64 encoded string.
+ */
+
+char *
+b64_encode (const unsigned char *, size_t);
+
+/**
+ * Decode `char *' source with `size_t' size.
+ * Returns a `unsigned char *' base64 decoded string + size of decoded string.
+ */
+unsigned char *
+b64_decode_ex (const char *, size_t, size_t *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+#ifdef b64_USE_CUSTOM_MALLOC
+extern void* b64_malloc(size_t);
+#endif
+
+#ifdef b64_USE_CUSTOM_REALLOC
+extern void* b64_realloc(void*, size_t);
+#endif
+
+// The number of buffers we need
+int bufc = 0;
+
+char* b64_buf_malloc()
+{
+	char* buf = b64_malloc(B64_BUFFER_SIZE);
+	bufc = 1;
+	return buf;
+}
+
+char* b64_buf_realloc(unsigned char* ptr, size_t size)
+{
+	if (size > bufc * B64_BUFFER_SIZE)
+	{
+		while (size > bufc * B64_BUFFER_SIZE) bufc++;
+		char* buf = b64_realloc(ptr, B64_BUFFER_SIZE * bufc);
+		if (!buf) return NULL;
+		return buf;
+	}
+
+	return ptr;
+}
+
+#ifdef b64_USE_CUSTOM_MALLOC
+extern void* b64_malloc(size_t);
+#endif
+
+#ifdef b64_USE_CUSTOM_REALLOC
+extern void* b64_realloc(void*, size_t);
+#endif
+
+char *
+b64_encode (const unsigned char *src, size_t len) {
+  int i = 0;
+  int j = 0;
+  char *enc = NULL;
+  size_t size = 0;
+  unsigned char buf[4];
+  unsigned char tmp[3];
+
+  // alloc
+  enc = (char *) b64_buf_malloc();
+  if (NULL == enc) { return NULL; }
+
+  // parse until end of source
+  while (len--) {
+    // read up to 3 bytes at a time into `tmp'
+    tmp[i++] = *(src++);
+
+    // if 3 bytes read then encode into `buf'
+    if (3 == i) {
+      buf[0] = (tmp[0] & 0xfc) >> 2;
+      buf[1] = ((tmp[0] & 0x03) << 4) + ((tmp[1] & 0xf0) >> 4);
+      buf[2] = ((tmp[1] & 0x0f) << 2) + ((tmp[2] & 0xc0) >> 6);
+      buf[3] = tmp[2] & 0x3f;
+
+      // allocate 4 new byts for `enc` and
+      // then translate each encoded buffer
+      // part by index from the base 64 index table
+      // into `enc' unsigned char array
+      enc = (char *) b64_buf_realloc(enc, size + 4);
+      for (i = 0; i < 4; ++i) {
+        enc[size++] = b64_table[buf[i]];
+      }
+
+      // reset index
+      i = 0;
+    }
+  }
+
+  // remainder
+  if (i > 0) {
+    // fill `tmp' with `\0' at most 3 times
+    for (j = i; j < 3; ++j) {
+      tmp[j] = '\0';
+    }
+
+    // perform same codec as above
+    buf[0] = (tmp[0] & 0xfc) >> 2;
+    buf[1] = ((tmp[0] & 0x03) << 4) + ((tmp[1] & 0xf0) >> 4);
+    buf[2] = ((tmp[1] & 0x0f) << 2) + ((tmp[2] & 0xc0) >> 6);
+    buf[3] = tmp[2] & 0x3f;
+
+    // perform same write to `enc` with new allocation
+    for (j = 0; (j < i + 1); ++j) {
+      enc = (char *) b64_buf_realloc(enc, size + 1);
+      enc[size++] = b64_table[buf[j]];
+    }
+
+    // while there is still a remainder
+    // append `=' to `enc'
+    while ((i++ < 3)) {
+      enc = (char *) b64_buf_realloc(enc, size + 1);
+      enc[size++] = '=';
+    }
+  }
+
+  // Make sure we have enough space to add '\0' character at end.
+  enc = (char *) b64_buf_realloc(enc, size + 1);
+  enc[size] = '\0';
+
+  return enc;
+}
+
+
+//////// END OF B64 ENCODING CODE
+
 void zend_dump_ht(HashTable *ht)
 {
 	zend_ulong index;
@@ -66,7 +250,9 @@ void zend_dump_const(const zval *zv)
 			fprintf(stderr, " float(%g)", Z_DVAL_P(zv));
 			break;
 		case IS_STRING:
-			fprintf(stderr, " string(\"%s\")", Z_STRVAL_P(zv));
+		  char* str = Z_STRVAL_P(zv);
+		  fprintf(stderr, " string(\"%s\")", b64_encode(str,strlen(str)));
+		  //fprintf(stderr, " string(\"%s\")", Z_STRVAL_P(zv));
 			break;
 		case IS_ARRAY:
 			fprintf(stderr, " array(...)");
